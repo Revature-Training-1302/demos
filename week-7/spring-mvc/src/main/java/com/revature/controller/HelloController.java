360c278b9bd361b46d01af9d0bc7bf4363a72eba
RoryEiffe <roryeiffe8@gmail.com>
2022-09-13 10:27:48 -0400

    Pushing mvc demo so far

---
+++ week-7/spring-mvc/src/main/java/com/revature/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.revature.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// this annotation is a stereotype
+// the other stereotypes are service and repository
+// indicates the purpose of the class
+@Controller
+public class HelloController {
+
+    // this is a regular Java method, and it returns a string:
+    // what url maps to this particular method? We define that using the RequestMapping annotation:
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
