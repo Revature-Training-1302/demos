3e7d6b467e8fefda400234b38379a873ac00dda2
RoryEiffe <roryeiffe8@gmail.com>
2022-10-12 10:56:53 -0400

    Pushing Angular 1-way demo

--- week-11/review/src/app/one-way/one-way.component.ts
+++ week-11/review/src/app/one-way/one-way.component.ts
@@ -6,21 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./one-way.component.css']
 })
 export class OneWayComponent implements OnInit {
-  // this value will be placed on the page with string interpolation:
-  name:string = "Robert";
+  // We create this string and use string interpolation to display it on the page
+  name:string = "Bob";
+  // use 2-way binding to map this value to input
+  value:string = "";
+  // use property binding to bind this url to the src property of our img tag:
   img:string = "https://www.history.com/.image/t_share/MTY3NDg4OTU3NjQxMTQwMDEz/halloween-gettyimages-172988453.jpg";
+  // boolean to keep track of whether we show something:
   show:boolean = true;
-
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  alert_message() {
-    alert("Hello!")
+  // take in a message and alert that to the web page:
+  alert_hello(message:string) {
+    alert(`Hello, this alert is triggered, message : ${this.value}`);
   }
 
+  // change the "show" variable from true to false or vice versa:
   toggle_show() {
     this.show = !this.show;
   }
